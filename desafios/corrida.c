9f2bad3143dddfd973e8551fea0b54e8e63a7a28
João Fernandes <jguiborges@gmail.com>
2024-04-08 20:33:39 -0300

    Desafio Grid de Largada

--- desafios/corrida.c
+++ desafios/corrida.c
@@ -11,12 +11,28 @@ void lerVetor(int vetor[], int tamanho){
 int comparaVetores(int inicial[], int final[], int tamanho){
 
     int contador = 0;
+    for(int i = tamanho-1; i >= 0; i--){
+        // print usado para conferir as trocas de posicoes
+        // printf("%d --- %d %d %d %d %d\n", i, inicial[0], inicial[1], inicial[2], inicial[3], inicial[4]);
+        
+        // executa ate que a posicao seja igual a final
+        while(inicial[i] != final[i]){
+            // confere as ultrapassagem de tras para frente 
+            for(int j = i - 1; j >= 0; j--){
+                int antigo = inicial[i];
+                int novo = inicial[j];
+                inicial[i] = novo;
+                inicial[j] = antigo;
+                contador++; 
 
-    for(int i = 0; i < tamanho; i++){
-        if(inicial[i] != final[i]){
-            contador++;
+                // se chegar na posicao certa, para as ultrapassagens desta posicao
+                if(inicial[i] == final[i]){
+                    break;
+                }
+            }    
         }
     }
+
     return contador;
     
 }
@@ -24,7 +40,8 @@ int comparaVetores(int inicial[], int final[], int tamanho){
 int main (){
     int ultrapassagens;
     int numeroCompetidores;
-    
+
+    printf("Digite o numero de corredores: ");
     scanf("%d", &numeroCompetidores);
 
     int posicaoInicial[numeroCompetidores];
@@ -35,7 +52,7 @@ int main (){
 
     ultrapassagens = comparaVetores(posicaoInicial, posicaoFinal, numeroCompetidores);
 
-    printf("%d", ultrapassagens);
+    printf("Ocorreram %d ultrapassagens", ultrapassagens);
 
     return 0;
 }
